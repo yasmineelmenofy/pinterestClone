c3945c806203cd575919cd932e6e218f3853face
Yasmine <yasmineahmedali1@gmail.com>
2026-05-06 15:12:59 +0300

    add registerUser function in the authController

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import UserModel from "../models/User.model";
 import bycrpt from "bcrypt";
 import generateToken from "../utils/generateToken";
+import { ApiError } from "../utils/ApiError";
 
-export const RegisterUser = async (req: Request, res: Response): Promise<void> => {
-    try{
-  const { userName, email, password, profileImage } = req.body;
-  const found = await UserModel.findOne({ email: email });
-  if (found) {
-    res.status(400).json({ message: "The user already exist" });
-    return;
-  }
-  const hash = await bycrpt.hash(password, 10);
+export const RegisterUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { userName, email, password, profileImage } = req.body;
+    const found = await UserModel.findOne({ email: email });
+    if (found) {
+      throw new ApiError("The user already exist", 400);
+    }
+    const hash = await bycrpt.hash(password, 10);
     const user = await UserModel.create({
       userName: userName,
       email: email,
@@ -33,7 +37,6 @@ export const RegisterUser = async (req: Request, res: Response): Promise<void> =
       },
     });
   } catch (error) {
-    res.status(400).json({ message: "User creation failed" });
-    return;
+      next(error);
   }
 };
